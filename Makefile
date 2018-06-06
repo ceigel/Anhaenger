4138351154f6b7180be45f10c0d2695d2bbec678
Cristian Eigel <cristian.eigel@icloud.com>
2018-06-06 21:30:42 +0200

    First version

---
+++ Makefile
@@ -0,0 +1,5 @@
+ARDUINO_DIR   = /Applications/Arduino.app/Contents/Java
+ARDMK_DIR     = /usr/local/opt/arduino-mk/
+MONITOR_PORT  = /dev/cu.usbmodem411
+BOARD_TAG     = uno
+include $(ARDMK_DIR)/Arduino.mk
---
+++ schedule.h
@@ -0,0 +1,165 @@
+#ifndef __SCHEDULE_H_
+#define __SCHEDULE_H_
+
+#include <avr/sleep.h>
+
+static constexpr uint16_t TickPrecision = 15;
+class Task
+{
+  protected:
+    enum class State {
+      Waiting,
+      Running
+    };
+    State current_state;
+    virtual bool should_run() {
+      return true;
+    }
+    virtual unsigned long step() {
+      return 0;
+    }
+  public:
+    void play() {
+      current_state = State::Running;
+    }
+    void pause() {
+      current_state = State::Waiting;
+    }
+    Task(State start_state) : current_state(start_state) {}
+    virtual unsigned long run() {
+      if(current_state == State::Running) {
+        return step();
+      }
+      else if(should_run() == true) {
+        current_state = State::Running;
+        return step();
+      }
+      return 1;
+    }
+    virtual void setup() {}
+
+    virtual void stop() {}
+};
+
+class Scheduler
+{
+  private:
+    struct TaskNode
+    {
+      Task* task;
+      unsigned long delay;
+      TaskNode* next;
+      TaskNode(Task* t, unsigned long dl, TaskNode* n)
+        : task(t), delay(dl), next(n) {}
+    };
+    void update_delays() {
+      auto current_millis = millis();
+      if(current_millis == prev_millis) {
+        return;
+      }
+      auto diff = current_millis - prev_millis;
+      prev_millis = current_millis;
+      TaskNode* n = tasks;
+      while(n) {
+        if(n->delay > diff) {
+          n->delay -= diff;
+        } else {
+          n->delay = 0;
+        }
+        n = n->next;
+      }
+    }
+    TaskNode* get_next_task() {
+      update_delays();
+      if(tasks && tasks->delay == 0) {
+        auto n = tasks;
+        tasks = tasks->next;
+        n->next = nullptr;
+        return n;
+      }
+      return nullptr;
+    }
+    void add_task_sorted(TaskNode* tsk) {
+      if(!tasks || tasks->delay > tsk->delay) {
+        tsk->next = tasks;
+        tasks = tsk;
+      } else {
+        auto p = tasks;
+        auto n = tasks->next;
+        while(n && n->delay <= tsk->delay) {
+          p = n;
+          n = n->next;
+        }
+        tsk->next = n;
+        p->next = tsk;
+      }
+    }
+
+    void stop_tasks() {
+      TaskNode *p = tasks;
+      while(p) {
+        p->task->stop();
+        p = p->next;
+      }
+    }
+
+    void sleep_now()         // here we put the arduino to sleep
+    {
+        set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
+        sleep_enable();          // enables the sleep bit in the mcucr register
+                                 // so sleep is possible. just a safety pin
+        sleep_mode();            // here the device is actually put to sleep!!
+                                 // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
+        sleep_disable();         // first thing after waking from sleep:
+                                 // disable sleep...
+    }
+    TaskNode* tasks;
+    TaskNode* unscheduled;
+    unsigned long prev_millis;
+  public:
+    Scheduler() : tasks(nullptr), unscheduled(nullptr), prev_millis(millis()) {
+    }
+
+    Scheduler(Task* ptasks[]) : tasks(nullptr), unscheduled(nullptr), prev_millis(millis()) {
+      for(unsigned int i = sizeof(ptasks)/sizeof(ptasks[0]); i > 0; --i) {
+        add_task(*ptasks[i-1]);
+      }
+
+    }
+    void add_task(Task& task) {
+      tasks = new TaskNode { &task, 0, tasks};
+    }
+
+    void setup_tasks() {
+      TaskNode *p = tasks;
+      while(p) {
+        p->task->setup();
+        p = p->next;
+      }
+    }
+    void run_next() {
+      TaskNode* tsk = get_next_task();
+      while(tsk)
+      {
+        auto next_delay = tsk->task->run();
+        if(next_delay == static_cast<unsigned long>(-1)) {
+          tsk->next = unscheduled;
+          unscheduled = tsk;
+        }
+        else {
+          tsk->delay = next_delay;
+          add_task_sorted(tsk);
+        }
+        tsk = get_next_task();
+      }
+      if(tasks->delay > 0) {
+        delay(tasks->delay);
+      }
+    }
+    void sleep() {
+      stop_tasks();
+      sleep_now();
+    }
+};
+
+#endif
